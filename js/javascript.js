117d351c0922281f3f85ebb48a01d501fda753ba
cristoay <cristo.ay@gmail.com>
2021-02-16 09:32:46 +0100

    jquery  funcional sin post

--- js/javascript.js
+++ js/javascript.js
@@ -34,35 +34,38 @@ $("#lista").click(function (event) {
 /**
  * Crear el main de la pagina 
  */
-function crearMain(){
+function crearMain() {
     var all = document.createElement("div");
     var cabecera = document.createElement("div");
-    cabecera.id="cabecera"
+    cabecera.id = "cabecera"
     var nuevaTienda = document.createElement("p");
-    nuevaTienda.id ="mostrarForm";
-    nuevaTienda.textContent="Nueva Tienda";
+    nuevaTienda.id = "mostrarForm";
+    nuevaTienda.textContent = "Nueva Tienda";
     var buscador = document.createElement("div");
     var idTienda = document.createElement("input");
-    idTienda.placeholder="Id de la Tienda";
+    idTienda.placeholder = "Id de la Tienda";
+    idTienda.id = "inputIdTienda";
     var btnBuscador = document.createElement("button");
+    btnBuscador.addEventListener("click", cargarIdjquery);
+    btnBuscador.id="find"
     buscador.appendChild(idTienda);
     buscador.appendChild(btnBuscador);
     var iconBuscadorTemp = document.getElementById("buscarIcon");
-    
+
     var iconBuscador = iconBuscadorTemp.content.cloneNode(true);
     console.log(iconBuscador.firstElementChild);
-    iconBuscador.firstElementChild.id="buscadorIcono";
+    iconBuscador.firstElementChild.id = "buscadorIcono";
     //iconBuscar ocultar cuando se empieze a realizar una petición de carga
     var iconCancelarTemp = document.getElementById("cancelarIcon");
     var iconCancelar = iconCancelarTemp.content.cloneNode(true);
-    iconCancelar.firstElementChild.id="cancelarIcono";
+    iconCancelar.firstElementChild.id = "cancelarIcono";
     //iconCancelar cambiar cuando se cargue la id tienda y se haya completado la petición
-    iconCancelar.firstElementChild.style.display="none";
+    iconCancelar.firstElementChild.style.display = "none";
     var iconLoaderTemp = document.getElementById("loaderIcon");
     var iconLoader = iconLoaderTemp.content.cloneNode(true);
-    iconLoader.firstElementChild.id="loaderIcono";
+    iconLoader.firstElementChild.id = "loaderIcono";
     //iconLoader poner cuando se realize la carga de la busqueda de la tienda por id 
-    iconLoader.firstElementChild.style.display="none";
+    iconLoader.firstElementChild.style.display = "none";
     btnBuscador.appendChild(iconBuscador);
     btnBuscador.appendChild(iconCancelar);
     btnBuscador.appendChild(iconLoader);
@@ -70,37 +73,37 @@ function crearMain(){
     cabecera.appendChild(buscador);
     all.appendChild(cabecera);
     var ocultador = document.createElement("div");
-    ocultador.id="ocultarForm";
+    ocultador.id = "ocultarForm";
     var formul = document.createElement("div");
-    formul.id="formulario";
+    formul.id = "formulario";
     var titulo = document.createElement("h2");
-    titulo.textContent ="Nueva empresa";
+    titulo.textContent = "Nueva empresa";
     formul.appendChild(titulo);
     var formulariotag = document.createElement("form");
-    var nomdiv = crearInput("Nombre","nombre","Nombre de la empresa");
-    var direcciondiv = crearInput("Dirección","direccion","Dirección de la empresa");
-    var localidaddiv = crearInput("Localidad","localidad","Localidad de la empresa");
-    var telefonodiv = crearInput("Teléfono","telefono","Teléfono de la empresa");
-    telefonodiv.firstElementChild.nextElementSibling.pattern ="^[689]\d{8}$";
+    var nomdiv = crearInput("Nombre", "nombre", "Nombre de la empresa");
+    var direcciondiv = crearInput("Dirección", "direccion", "Dirección de la empresa");
+    var localidaddiv = crearInput("Localidad", "localidad", "Localidad de la empresa");
+    var telefonodiv = crearInput("Teléfono", "telefono", "Teléfono de la empresa");
+    telefonodiv.firstElementChild.nextElementSibling.pattern = "^[689]\d{8}$";
     var botondiv = document.createElement("div");
     botondiv.id = "boton";
     var boton = document.createElement("button");
     boton.id = "aniadirTienda";
     var cargarForm = document.createElement("div");
-    cargarForm.id="cargarPost";
+    cargarForm.id = "cargarPost";
     var cargandoForm = document.createElement("div");
-    cargandoForm.id="cargandoPost";
-    cargandoForm.textContent="Cargando";
+    cargandoForm.id = "cargandoPost";
+    cargandoForm.textContent = "Cargando";
     var baseForm = document.createElement("div");
-    baseForm.id="baseForm";
-    baseForm.textContent="Añadir tienda";
+    baseForm.id = "baseForm";
+    baseForm.textContent = "Añadir tienda";
     var iconLoaderForm = iconLoaderTemp.content.cloneNode(true);
-    iconLoaderForm.firstElementChild.id="loaderIcono";
+    iconLoaderForm.firstElementChild.id = "loaderIcono";
     cargarForm.appendChild(iconLoaderForm);
     cargarForm.appendChild(cargandoForm);
     boton.appendChild(cargarForm);
     //carform boton del formulario para dar feedback mientras se envia la peticion
-    cargarForm.style.display="none";
+    cargarForm.style.display = "none";
     boton.appendChild(baseForm);
     botondiv.appendChild(boton);
     formulariotag.appendChild(nomdiv);
@@ -111,37 +114,37 @@ function crearMain(){
     formul.appendChild(formulariotag);
     ocultador.appendChild(formul);
     var result = document.createElement("div");
-    result.id="resultados";
+    result.id = "resultados";
     all.appendChild(ocultador);
     all.appendChild(result);
     document.getElementsByTagName("main")[0].appendChild(all);
 
 }
 
-function crearInput(contenido , id , placeholder){
+function crearInput(contenido, id, placeholder) {
     var div = document.createElement("div");
     var error = document.createElement("p");
-    error.classList="rojo";
+    error.classList = "rojo";
     var labdiv = document.createElement("label");
-    labdiv.textContent=contenido;
-    labdiv.htmlFor=id;
+    labdiv.textContent = contenido;
+    labdiv.htmlFor = id;
     var inputdiv = document.createElement("input");
-    inputdiv.type="text";
-    inputdiv.id=id;
-    inputdiv.placeholder=placeholder;
+    inputdiv.type = "text";
+    inputdiv.id = id;
+    inputdiv.placeholder = placeholder;
     inputdiv.required = true;
     div.appendChild(labdiv);
     div.appendChild(inputdiv);
     div.appendChild(error);
     return div;
-    
+
 }
 
 var template = document.getElementById("templateLista");
 var principal = document.getElementsByTagName("main");
 document.getElementById("jqueryButton").addEventListener("click", cargarTodojquery);
 function loadjquery() {
-    
+
     crearMain();
     mostrarForm = document.getElementById("mostrarForm");
     console.log(mostrarForm);
@@ -176,7 +179,7 @@ function loadjquery() {
     })
 
 }
-function cargarTodojquery(){
+function cargarTodojquery() {
     borrarMain();
     $("#spinnerContainerMain").show();
     $.ajax({
@@ -187,63 +190,97 @@ function cargarTodojquery(){
         success: function (data) {
             loadjquery();
             $("#spinnerContainerMain").hide();
-            console.log("response:" + JSON.stringify(data));
+            //console.log("response:" + JSON.stringify(data));
             $.each(data, function (j, pdata) {
                 var empresaTemp = document.getElementById("empresaContainer");
                 var empr = empresaTemp.content.cloneNode(true);
-                console.log(empr.firstElementChild)
-                empr.firstElementChild.firstElementChild.textContent=pdata.nombreTienda;
-                empr.firstElementChild.firstElementChild.nextElementSibling.textContent=pdata.direccion+"("+pdata.localidad+")";
-                empr.firstElementChild.firstElementChild.nextElementSibling.nextElementSibling.textContent=pdata.telefono;
-                console.log(pdata);
+                //console.log(empr.firstElementChild)
+                empr.firstElementChild.firstElementChild.textContent = pdata.nombreTienda;
+                empr.firstElementChild.firstElementChild.nextElementSibling.textContent = pdata.direccion + "(" + pdata.localidad + ")";
+                empr.firstElementChild.firstElementChild.nextElementSibling.nextElementSibling.textContent = pdata.telefono;
+                //console.log(pdata);
                 document.getElementById("resultados").appendChild(empr);
             });
-            
+
 
         },
         error: function (jqXHR, textStatus, errorThrown) {
             console.log(' Error in processing! ' + textStatus);
         }
+        
     });
+    
 }
-function cargarIdjquery(){
+function cargarIdjquery() {
     $("#buscadorIcono").hide();
-   // $("#")
+    $("#loaderIcono").show();
+    $("#resultados").empty();
+    var valor = document.getElementById("inputIdTienda");
+
     $.ajax({
         type: "GET",
         dataType: "json",
-        url: "https://webapp-210130211157.azurewebsites.net/webresources/mitienda/",
+        url: "https://webapp-210130211157.azurewebsites.net/webresources/mitienda/" + valor.value,
 
         success: function (data) {
-            $("#spinnerContainerMain").hide();
+            $("#loaderIcono").hide();
+            $("#cancelarIcono").show();
             console.log("response:" + JSON.stringify(data));
-            $.each(data, function (j, pdata) {
-                console.log(pdata);
-            });
-            loadjquery();
+            if (data === undefined) {
+                var notFound = document.createElement("h1");
+                notFound.textContent = "Tienda no encontrada"
+                document.getElementById("resultados").appendChild(notFound);
+
+            } else if (data.length){
+                var notFound = document.createElement("h1");
+                notFound.textContent = "Insertar un id antes de usar el botón de busqueda"
+                document.getElementById("resultados").appendChild(notFound);
+            }
+                else{
+                console.log(data.length);
+                console.log(data);
+                var empresaTemp = document.getElementById("empresaContainer");
+                var empr = empresaTemp.content.cloneNode(true);
+                //console.log(empr.firstElementChild)
+                empr.firstElementChild.firstElementChild.textContent = data.nombreTienda;
+                empr.firstElementChild.firstElementChild.nextElementSibling.textContent = data.direccion + "(" + data.localidad + ")";
+                empr.firstElementChild.firstElementChild.nextElementSibling.nextElementSibling.textContent = data.telefono;
+                //console.log(pdata);
+                document.getElementById("resultados").appendChild(empr);
+            }
+
+
+
 
         },
         error: function (jqXHR, textStatus, errorThrown) {
-            console.log(' Error in processing! ' + textStatus);
+            $("#loaderIcono").hide();
+            $("#cancelarIcono").show();
+            var notFound = document.createElement("h1");
+            notFound.textContent = "Tienda no encontrada"
+            document.getElementById("resultados").appendChild(notFound);
         }
     });
+    var btnCancelar = document.getElementById("find");
+    btnCancelar.removeEventListener("click",cargarIdjquery);
+    btnCancelar.addEventListener("click", cargarTodojquery);
 }
 
 function increaseHeight() {
     var formulario = document.getElementById("ocultarForm");
-    formulario.style.maxHeight = "0px";
+    formulario.style.maxHeight = "350px";
     mostrarForm.removeEventListener('click', increaseHeight);
     mostrarForm.addEventListener('click', reduceHeight);
 
 }
 function reduceHeight() {
     var formulario = document.getElementById("ocultarForm");
-    formulario.style.maxHeight = "350px";
+    formulario.style.maxHeight = "0px";
     mostrarForm.removeEventListener('click', reduceHeight);
     mostrarForm.addEventListener('click', increaseHeight);
 }
 
-function borrarMain(){
+function borrarMain() {
     while (document.getElementsByTagName("main")[0].firstChild) {
         document.getElementsByTagName("main")[0].firstChild.remove()
     }
